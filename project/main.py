4ed2f48e89cc746ad93b183fe24bda26f53c84b5
Bartłomiej Gasyna <bartlomiej.gasyna@student.put.poznan.pl>
2023-05-31 00:41:19 +0200

    IT WORKS

--- project/main.py
+++ project/main.py
@@ -2,9 +2,13 @@
 import cv2
 from utils.load_data import load_data
 
-# import torch
-# import numpy as np
-from utils.dataTyes import IoU, compute_intersection
+from pgmpy.models import FactorGraph
+from pgmpy.factors.discrete import DiscreteFactor
+from pgmpy.inference import BeliefPropagation
+from itertools import combinations
+import numpy as np
+from utils.dataTyes import compute_intersection, compare_histograms
+from collections import OrderedDict
 
 # dir = 'data/frames/'
 
@@ -22,11 +26,11 @@ for idx, frame in enumerate(frames):
     # show bboxes
     for bbox in frame.bboxes:
         x, y, w, h = list(map(int, bbox))
-        cv2.rectangle(img_curr, (x, y), (x+w, y+h), (0, 255, 0) )
+        cv2.rectangle(img_curr, (x, y), (x+w, y+h), (0, 0, 100) )
 
     for bbox in frame_prev.bboxes:
         x, y, w, h = list(map(int, bbox))
-        cv2.rectangle(img_curr, (x, y), (x+w, y+h), (0, 255, 0) )
+        cv2.rectangle(img_curr, (x, y), (x+w, y+h), (0, 0, 255) )
     
     # iou_, intersection_boxes = compute_intersection(frame.bboxes, frame_prev.bboxes)
     # print('ious ')
@@ -34,22 +38,86 @@ for idx, frame in enumerate(frames):
     #     cv2.rectangle(img_curr, (int(box[0]), int(box[1])), (int(box[0]+box[2]), int(box[1]+box[3])), (0, 0, 255))
     #     print(iou)
 
-    frame.histograms()
-
-
-
-    cv2.imshow('frame', img_curr)
-    # cv2.imshow('prev', frame_prev.img())
-
-    key = cv2.waitKey()
-    if key == ord('q'):
-        cv2.destroyAllWindows
-        break
-
     ###############
     #  TODO: Process 
     ###############
     # process(frame, frame_prev)
 
+    cv2.imshow('frame', img_curr)
+    key = cv2.waitKey()
+    if key == ord('q'):
+        cv2.destroyAllWindows
+        break
+
+    G = FactorGraph()
+
+    # no bboxes in frame
+    if frame.n == 0:
+        print()
+        continue
+    
+    # if no bboxes in previous frame, for each bbox in current frame, print '-1'
+    if frame_prev.n == 0:
+        print(' '.join(['-1'] * frame.n))
+        continue
+    # number of bboxes defines number of nodes
+    for i in range(frame.n):
+        G.add_node(str(i))
+
+
+    compare_histograms(frame, frame_prev, G)
+
+    ##############
+    # M =
+    # [[1. 1. 1.]
+    # [1. 0. 1.]
+    # [1. 1. 0.]
+    # [1. 1. 1.]
+    # [1. 1. 1.]]
+    ##############
+
+    # M = np.ones((frame_prev.n + 1, frame.n + 1), dtype=float)
+    # np.fill_diagonal(M, 0)
+    # M[0,0] = 1
+
+    M = np.ones((len(frame_prev.hists)+1,len(frame_prev.hists)+1))
+
+    for i in range(len(frame_prev.hists)+1):
+        for j in range(len(frame_prev.hists)+1):
+            if i != 0:
+                if i == j:
+                    M[i][j] = 0
+
+    # current_histrogram1 = 0
+    # current_histrogram2 = 1
+    for current_histrogram1, current_histrogram2 in combinations(range(int(frame.n)), 2):
+        # print('loop')
+        # print([str(current_histrogram1), str(current_histrogram2)])
+        # print()
+        # print([frame_prev.n + 1, frame_prev.n + 1])
+        # print()
+        # print(M)
+        tmp = DiscreteFactor([str(current_histrogram1), str(current_histrogram2)], [frame_prev.n + 1,
+                                                                                frame_prev.n + 1],
+                                                                                M)
+        G.add_factors(tmp)
+        G.add_edge(str(current_histrogram1), tmp)
+        G.add_edge(str(current_histrogram2), tmp)
+
+
+    # Belief propagation
+    BP = BeliefPropagation(G)
+    BP.calibrate()
+
+    pre_results = (BP.map_query(G.get_variable_nodes(), show_progress=False))
+    pre_results2 = OrderedDict(sorted(pre_results.items()))
+
+    result = list(pre_results2.values())
+    final = []
+
+    for res in result:
+        value = res - 1
+        final.append(value)
+    print(*final, sep = ' ')
     ###############
     frame_prev = frame
--- project/utils/dataTyes.py
+++ project/utils/dataTyes.py
@@ -1,5 +1,10 @@
 import cv2
 import matplotlib.pyplot as plt
+
+from pgmpy.models import FactorGraph
+from pgmpy.factors.discrete import DiscreteFactor
+from pgmpy.inference import BeliefPropagation
+
 class Frame():
     ''' Frame class stores information about frame filename, number of bboxes, bbox coordinates'''
     def __init__(self, dir_name: str, filename: str, n: int, bboxes: list):
@@ -8,6 +13,7 @@ class Frame():
         self.n = n
         self.bboxes = bboxes
         
+        self.hists = []
     def img(self):
         return cv2.imread(self.dir_name + self.filename)
     
@@ -35,7 +41,10 @@ class Frame():
             # Calculate the histogram of the center region
             center_roi = cv2.cvtColor(center_roi, cv2.COLOR_BGR2HSV)
 
-            hist = cv2.calcHist([center_roi], [0], None, [256], [0, 256])
+            h, s, _ = cv2.split(center_roi)
+            center_roi_selected_channels = cv2.merge([h, s])
+
+            hist = cv2.calcHist([center_roi_selected_channels], [0], None, [256], [0, 256])
 
                 # Normalize the histogram
             # hist /= center_w * center_h
@@ -43,11 +52,12 @@ class Frame():
 
 
             hists.append(hist)
+            
 
             # plt.plot(hist)
             # plt.show()
             # print('histogram: ', hist)
-            
+        self.hists = hists
         return hists
 
 class FrameList(list):
@@ -58,28 +68,6 @@ class FrameList(list):
             raise TypeError("Only instances of Frame class can be added to FrameList")
 
 
-def IoU(boxA, boxB):
-        
-
-
-	# determine the (x, y)-coordinates of the intersection rectangle
-	xA = max(boxA[0], boxB[0])
-	yA = max(boxA[1], boxB[1])
-	xB = min(boxA[0] + boxA[2], boxB[0] + boxB[2])
-	yB = min(boxA[1] + boxA[3], boxB[1] + boxB[3])
-        
-	# # compute the area of intersection rectangle
-	# interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-	# # compute the area of both the prediction and ground-truth
-	# # rectangles
-	# boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-	# boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-	# # compute the intersection over union by taking the intersection
-	# # area and dividing it by the sum of prediction + ground-truth
-	# # areas - the interesection area
-	# iou = interArea / float(boxAArea + boxBArea - interArea)
-	# # return the intersection over union value
-	return (int(xA), int(yA)), (int(xB), int(yB))
 
 def compute_intersection(bboxA, bboxB):
     iou_ = []
@@ -102,3 +90,53 @@ def compute_intersection(bboxA, bboxB):
                 
     return iou_, boxes
 
+# TODO: histograms can be optimized by saving them
+def compare_histograms(frame1: Frame, frame2: Frame, G: FactorGraph):
+    histograms_current = frame1.histograms()
+    histograms_previous = frame2.histograms()
+
+
+    hists_similarity = []
+    for idx, current_hist in enumerate(histograms_current):
+        hist_similarity = []
+        for prev_hist in histograms_previous:
+            sim = cv2.compareHist(current_hist, prev_hist, cv2.HISTCMP_CORREL)
+            # hist_similarity.append(cv2.compareHist(current_hist, prev_hist, cv2.HISTCMP_CORREL))
+            hist_similarity.append(sim)
+    
+    # print('similarity: ', hist_similarity) # DEBUG only
+
+    # Adding factors to graph
+        # print([len(histograms_previous) + 1])
+        # print([[0.29] + hist_similarity])
+        tmp = DiscreteFactor([str(idx)], [frame2.n + 1], [[0.29] + hist_similarity])
+        G.add_factors(tmp)
+        G.add_edge(str(idx), tmp)
+
+    return hist_similarity
+
+# def compare_histograms(frame1: Frame, frame2: Frame, G: FactorGraph):
+#     hist1 = frame1.histograms()
+#     hist2 = frame2.histograms()
+
+# # In this function, histogram from both previous and current frame are compared against each other. Comparison value is
+# # a mean value for comparing considered channels. For comparing I used ready Bhattacharyya method.
+
+#     sum = 0
+#     for i in range(len(hist1)):
+#         mean_j = []
+#         sum = 0
+#         for j in range(len(hist2)):
+#             comparison1 = cv2.compareHist(hist1[i][0],hist2[j][0],cv2.HISTCMP_BHATTACHARYYA)
+#             comparison2 = cv2.compareHist(hist1[i][1],hist2[j][1],cv2.HISTCMP_BHATTACHARYYA)
+#             # comparison3 = cv2.compareHist(hist1[i][2],hist2[j][2],cv2.HISTCMP_BHATTACHARYYA)
+#             comparison = (comparison1 + comparison2) / 2
+#             comparison = 1 - comparison
+#             mean_j.append(comparison)
+
+# # Adding DiscreteFactor to the FactorGraph()
+#         tmp = DiscreteFactor([str(i)], [len(hist2) + 1], [[0.29] + mean_j])
+#         G.add_factors(tmp)
+#         G.add_edge(str(i),tmp)
+
+#     return mean_j
